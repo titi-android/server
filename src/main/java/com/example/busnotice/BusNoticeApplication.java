5b405d1f9eda06672dcc741712d87989e0583649
donghyuun <bomul0524@gmail.com>
2025-01-22 02:55:25 +0900

    feat: 타임존 설정

--- src/main/java/com/example/busnotice/BusNoticeApplication.java
+++ src/main/java/com/example/busnotice/BusNoticeApplication.java
@@ -1,5 +1,7 @@
 package com.example.busnotice;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -7,6 +9,11 @@ import org.springframework.cache.annotation.EnableCaching;
 @EnableCaching
 @SpringBootApplication
 public class BusNoticeApplication {
+
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BusNoticeApplication.class, args);
