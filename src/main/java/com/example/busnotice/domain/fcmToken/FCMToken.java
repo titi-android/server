99d0216204ca93ca85466e3f06ec28a826fa3d16
donghyuun <bomul0524@gmail.com>
2025-01-22 00:46:24 +0900

    fix: fcm 토큰 엔티티 누락 해결

---
+++ src/main/java/com/example/busnotice/domain/fcmToken/FCMToken.java
@@ -0,0 +1,40 @@
+package com.example.busnotice.domain.fcmToken;
+
+import com.example.busnotice.domain.user.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.Data;
+
+@Entity
+@Data
+public class FCMToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column
+    private String token;
+
+    public FCMToken() {
+
+    }
+
+    public void update(String token) {
+        this.token = token;
+    }
+
+    public FCMToken(User user, String token) {
+        this.user = user;
+        this.token = token;
+    }
+}
