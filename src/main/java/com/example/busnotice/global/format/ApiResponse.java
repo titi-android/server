6931e3a4420faae8a3ff4ca173fd8515b793fb02
donghyuun <bomul0524@gmail.com>
2025-02-05 12:58:37 +0900

    feat: ApiResponse 에 isSuccess 필드 추가

--- src/main/java/com/example/busnotice/global/format/ApiResponse.java
+++ src/main/java/com/example/busnotice/global/format/ApiResponse.java
@@ -6,50 +6,55 @@ import lombok.Getter;
 @Getter
 public class ApiResponse<T> {
 
-    private int status;
+    private boolean isSuccess;
+    private int code;
     private T data;
     private String message;
 
     public static <T> ApiResponse<T> createSuccess() {
-        return new ApiResponse<>(StatusCode.OK);
+        return new ApiResponse<>(true, StatusCode.OK);
     }
 
     public static <T> ApiResponse<T> createSuccess(String message) {
-        return new ApiResponse<>(StatusCode.OK, message);
+        return new ApiResponse<>(true, StatusCode.OK, message);
     }
 
     public static <T> ApiResponse<T> createSuccessWithData(T data) {
-        return new ApiResponse<>(StatusCode.OK, data);
+        return new ApiResponse<>(true, StatusCode.OK, data);
     }
 
     public static <T> ApiResponse<T> createSuccessWithData(T data, String message) {
-        return new ApiResponse<>(StatusCode.OK, data, message);
+        return new ApiResponse<>(true, StatusCode.OK, data, message);
     }
 
     public static <T> ApiResponse<T> createFail(StatusCode status, String message) {
-        return new ApiResponse<>(status, message);
+        return new ApiResponse<>(false, status, message);
     }
 
-    private ApiResponse(StatusCode status) {
-        this.status = status.getCode();
+    private ApiResponse(Boolean isSuccess, StatusCode status) {
+        this.isSuccess = isSuccess;
+        this.code = status.getCode();
         this.data = null;
         this.message = null;
     }
 
-    private ApiResponse(StatusCode status, String message) {
-        this.status = status.getCode();
+    private ApiResponse(Boolean isSuccess, StatusCode status, String message) {
+        this.isSuccess = isSuccess;
+        this.code = status.getCode();
         this.data = null;
         this.message = message;
     }
 
-    private ApiResponse(StatusCode status, T data) {
-        this.status = status.getCode();
+    private ApiResponse(Boolean isSuccess, StatusCode status, T data) {
+        this.isSuccess = isSuccess;
+        this.code = status.getCode();
         this.data = data;
         this.message = null;
     }
 
-    private ApiResponse(StatusCode status, T data, String message) {
-        this.status = status.getCode();
+    private ApiResponse(Boolean isSuccess, StatusCode status, T data, String message) {
+        this.isSuccess = isSuccess;
+        this.code = status.getCode();
         this.data = data;
         this.message = message;
     }
