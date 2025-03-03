0e765d188a8a47882b808d6fe9e95757f8719d0f
donghyuun <bomul0524@gmail.com>
2025-03-03 20:16:41 +0900

    refactor: 로그에 fcm 내용 출력

--- src/main/java/com/example/busnotice/domain/fcmToken/FCMService.java
+++ src/main/java/com/example/busnotice/domain/fcmToken/FCMService.java
@@ -115,11 +115,12 @@ public class FCMService {
             try {
                 String response = FirebaseMessaging.getInstance().send(message);
                 log.info("메시지 전송 성공: {}, 토큰: {}", response, notification.token());
+                log.info("메시지 내용: {}", message.toString());
             } catch (FirebaseMessagingException e) {
                 log.error("메시지 전송 실패: {}, 토큰: {}", e.getMessage(), notification.token());
+                log.error("메시지 내용: {}", message.toString());
             }
         }
     }
-
 
 }
